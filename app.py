1d12d7f88fcee10a4180fb3490466c482b553e00
askvart <tur1amant@gmail.com>
2022-01-26 19:43:46 +0300

    home, about pages

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ from api import API
 
 
 app = API()
+
+def home(request, response):
+    response.text = "Hello frrom the Home page"
+
+def about(request, response):
+    response.text = "Hello from the about page"
