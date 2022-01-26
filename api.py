204b0de812cf4fda56ba9a36916ec20075ce080d
askvart <tur1amant@gmail.com>
2022-01-26 19:41:10 +0300

    user_agent

--- api.py
+++ api.py
@@ -3,7 +3,13 @@ from webob import Request, Response
 class API:
     def __call__(self, environ, start_response):
         request = Request(environ)
-        response = Response()
-        response.text = "Hello, Askvart!"
+        response = self.handle_request(request)
 
         return response(environ, start_response)
+
+    def handle_request(self, request):
+        user_agent = request.environ.get("HTTP_USER_AGENT", "NO User Agent Found")
+        response = Response()
+        response.text = f"Hello, my friend with this user agent: {user_agent}"
+
+        return response
