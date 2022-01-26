283672681bfb3b4c793390059c05f741310d03d8
askvart <tur1amant@gmail.com>
2022-01-26 19:51:57 +0300

    def route(), change def handle_request()

--- api.py
+++ api.py
@@ -1,6 +1,15 @@
 from webob import Request, Response
 
 class API:
+    def __init__(self):
+        self.routes = {}
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        return wrapper
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
@@ -8,8 +17,10 @@ class API:
         return response(environ, start_response)
 
     def handle_request(self, request):
-        user_agent = request.environ.get("HTTP_USER_AGENT", "NO User Agent Found")
         response = Response()
-        response.text = f"Hello, my friend with this user agent: {user_agent}"
+        for path, handler in self.routes.items():
+            if path == request.path:
+                handler(request, response)
+                return response
 
         return response
--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from api import API
 
 app = API()
 
+@app.route("/home")
 def home(request, response):
     response.text = "Hello frrom the Home page"
 
+@app.route("/about")
 def about(request, response):
     response.text = "Hello from the about page"
